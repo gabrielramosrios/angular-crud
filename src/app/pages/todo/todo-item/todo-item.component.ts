3a8383ace8ff5dd20818bf0b80fa2370e609474a
Gabriel Ramos Rios <gabriel.ramos.rios@hotmail.com>
2022-07-20 23:38:01 -0300

    add todo component

---
+++ src/app/pages/todo/todo-item/todo-item.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo = {
+    id: 0,
+    title: '',
+    done: false,
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  removeTodo(): void  {
+    console.log(this.todo)
+
+  } 
+
+  markAsDone (): void  {
+    console.log(this.todo)
+  }
+}
---
+++ src/app/pages/todo/todo-list/todo-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+  todos: Array<Todo> = [];
+  todo: Todo = {
+    id: 0,
+    title: '',
+    done: false,
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+ addTodo(title: string) {
+   this.todo.id = this.todos.length +1;
+   this.todo.title = title;
+   this.todos.push(this.todo);
+   console.log(this.todos);
+ } 
+}
